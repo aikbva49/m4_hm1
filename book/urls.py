1020b0ff85895b79afeef967c367b577baf32369
aikbva49 <aikbva49@gmail.com>
2026-04-28 14:56:42 +0600

    hw1

--- book/urls.py
+++ book/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views 
 
 urlpatterns = [
-    path('q1/', views.q1, name='q1'),
-    path('q2/', views.q2, name='q2'),
-    path('q3/', views.q3, name='q3'),
+    path('qs1/', views.qs1, name='qs1'),
+    path('qs2/', views.qs2, name='qs2'),
+    path('qs3/', views.qs3, name='qs3'),
 ]
--- book/views.py
+++ book/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def q1(request):
+def qs1(request):
     return HttpResponse("«Боль неизбежна. Страдание – личный выбор каждого». -Харуки Мураками")
 
-def q2(request):
+def qs2(request):
     return HttpResponse("«Когда счастье есть, о нем не думают» -Чынгыз Айтматов")
 
-def q3(request):
+def qs3(request):
     return HttpResponse("«Смысл жизни в том, что она имеет свой конец.»-Франц Кафка")
